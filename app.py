5272955bba9ed95dad575d96f56803a7ca06a022
Cata VC <catalina.valenzuelac@pregrado.uoh.cl>
2021-06-14 16:11:57 -0400

    primera base del proyecto

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/calc")
+def admin():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run()
